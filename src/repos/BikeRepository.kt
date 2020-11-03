7fe8062fe46b42f7c8a2e337e0e37e84d1881ec1
Amer Harb <amer.harb@gmail.com>
2020-11-03 10:36:22 +0100

    refactor

--- src/repos/BikeRepository.kt
+++ src/repos/BikeRepository.kt
@@ -1,11 +1,7 @@
 package se.hkr.bikesharektor.repos
 
-import se.hkr.bikesharektor.models.Bike
-
 class BikeRepository {
-    fun getById(bikeId: Int): Bike? {
-        return MockDatabase.bikes.firstOrNull { it.id == bikeId }
-    }
+    fun getById(bikeId: Int) = MockDatabase.bikes.firstOrNull { it.id == bikeId }
 
     fun getAll() = MockDatabase.bikes
 }
